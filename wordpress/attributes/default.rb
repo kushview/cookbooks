d8b072b66832b58e7d447c1d88755ed71646b273
Michael Fisher <mfisher@kushview.net>
2015-06-06 14:33:46 -0400

    Added support for wordpress AWS keys in wp-config.php

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -3,3 +3,7 @@ default['wordpress']['wp_config']['enable_W3TC'] = false
 
 # Force logins via https (http://codex.wordpress.org/Administration_Over_SSL#To_Force_SSL_Logins_and_SSL_Admin_Access)
 default['wordpress']['wp_config']['force_secure_logins'] = false
+
+default['wordpress']['wp_config']['have_aws_keys'] = false
+default['wordpress']['wp_config']['aws_access_key'] = ''
+default['wordpress']['wp_config']['aws_secret_access_key'] = ''
--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -1,8 +1,6 @@
-name "figaro"
+name "wordpress"
 maintainer "kushview"
-description "Writes a config/application.yml file with custom ENV values to apps' deploy directories."
+description "Writes WP config files."
 version "0.1"
 
-recipe "opsworks_custom_env::configure", "Write custom configuration and notify rails application to restart upon changes."
-recipe "opsworks_custom_env::restart_command", "Helper recipe that defines command to restart rails application."
-recipe "opsworks_custom_env::write_config", "Write a config/application.yml file to app's deploy directory. Used by configure recipe, or can be used directly in custom situations."
+recipe "wordpress::configure", "Write custom configuration and notify."
