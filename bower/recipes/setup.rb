84f23763da677ea6719134cee9abda7a320e7e29
Michael R. Fisher <mfisher@kushview.net>
2018-06-10 21:45:47 -0400

    Update setup.rb

--- bower/recipes/setup.rb
+++ bower/recipes/setup.rb
@@ -3,7 +3,10 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping application #{application} as it is not a Rails app")
     next
   end
-
+  execute 'npm untrusted certs' do
+    command 'npm config set strict-ssl false'
+    user 'root'
+  end
   execute 'install bower' do
     command 'npm install -g bower'
     user 'root'
